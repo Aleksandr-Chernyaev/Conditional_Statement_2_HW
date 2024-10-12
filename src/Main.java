68dcb6e7f25ae69da919facf132ac80d6c486b27
Aleksandr-Chernyaev <whiteanapa140190@gmail.com>
2024-10-12 17:16:46 +0300

    Conditional_Statement_2_HW

--- src/Main.java
+++ src/Main.java
@@ -27,12 +27,10 @@ public class Main {
             System.out.println("Установите облегченную версию приложения для iOS по ссылке");
         } else if (clientOS == 0 && clientDeviceYear >= 2015) {
             System.out.println("Установите обычную версию приложения для iOS по ссылке");
-        } else if (clientOS == 1 && clientDeviceYear < 2015) {
+        } if (clientOS == 1 && clientDeviceYear < 2015) {
             System.out.println("Установите облегченную версию приложения для Android по ссылке");
         } else if (clientOS == 1 && clientDeviceYear >= 2015) {
             System.out.println("Установите обычную версию приложения для Android по ссылке");
-        } else {
-            System.out.println("Неверное значение");
         }
 
         // task 3
